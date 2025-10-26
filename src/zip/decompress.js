123c0e08bece3c29538fc8e306708e9c3f55c9f4
anastasiya.cubuk <anastasiya.askerka@workhuman.com>
2025-10-26 21:33:06 +0300

    new: [Zlib] compress, decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import zlib from 'zlib';
+
 const decompress = async () => {
-  // Write your code here
+    const src = 'src/zip/files/archive.gz';
+    const dest = 'src/zip/files/fileToCompress.txt';
+
+    try {
+        const readable = fs.createReadStream(src);
+        const writable = fs.createWriteStream(dest);
+        const gunzip = zlib.createGunzip();
+
+        readable.pipe(gunzip).pipe(writable);
+
+        readable.on('error', () => { throw new Error('FS operation failed'); });
+        writable.on('error', () => { throw new Error('FS operation failed'); });
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await decompress();
