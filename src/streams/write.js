7dc3dc3c95e67a317fe1eb67db29990b24286691
anastasiya.cubuk <anastasiya.askerka@workhuman.com>
2025-10-26 21:21:12 +0300

    new: [Stream] write, read, transform

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+
 const write = async () => {
-  // Write your code here
+    const filePath = 'src/streams/files/fileToWrite.txt';
+
+    try {
+        const writableStream = fs.createWriteStream(filePath);
+        process.stdin.pipe(writableStream);
+
+        writableStream.on('error', () => {
+            throw new Error('FS operation failed');
+        });
+    } catch {
+        throw new Error('FS operation failed');
+    }
 };
 
 await write();
