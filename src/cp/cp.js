b1aad5dc3dc357e7b5d3abb039597e3867458c1f
anastasiya.cubuk <anastasiya.askerka@workhuman.com>
2025-10-26 23:03:16 +0300

    [new] [CP] implement child process spawning and piping

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,26 @@
+import { spawn } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+    // create child process
+    const child = spawn('node', ['./src/cp/files/script.js', ...args], {
+        stdio: ['pipe', 'pipe', process.stderr] // stdin/ stdout of child process are piped
+    });
+
+    // redirect stdin of main process to stdin of child process
+    process.stdin.pipe(child.stdin);
+
+    // redirect stdout of child process to stdout of main process
+    child.stdout.pipe(process.stdout);
+
+    // error handling
+    child.on('error', (err) => {
+        console.error('Child process error:', err);
+    });
+
+    child.on('close', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['arg1', 'arg2']);
